1af5abf58eec0269031a9a10c39c2455f8e9ca98
Charan Raj <88527168+manchalacharan@users.noreply.github.com>
2023-03-01 22:35:57 +0530

    Add files via upload

---
+++ Swap_two_intergers.py
@@ -0,0 +1,15 @@
+'''
+Write a Python program to swap two variables
+Input:
+30
+20
+Output:
+30 20 20 30       '''
+
+import math
+
+a = int(input())
+b = int(input())
+print(a,b)
+a,b=b,a
+print(a,b)
