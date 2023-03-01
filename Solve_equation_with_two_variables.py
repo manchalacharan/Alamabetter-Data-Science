cbb58bdb280c2aecf0b13c2b986067c004c87935
Charan Raj <88527168+manchalacharan@users.noreply.github.com>
2023-03-01 22:29:00 +0530

    Add files via upload

---
+++ Solve_equation_with_two_variables.py
@@ -0,0 +1,16 @@
+'''
+Write a Python program to solve (x + y) * (x + y)
+Input:
+4
+3
+Output:
+49                      '''
+
+import math
+
+x = int(input())
+y = int(input())
+
+z = (x+y)*(x+y)
+
+print(z)
